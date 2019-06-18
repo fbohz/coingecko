97a0223bb6b661a6962003fcdd1e656960c5359d
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-06-18 03:51:30 +0000

    basic finder but needs refactor

--- lib/coingecko/api.rb
+++ lib/coingecko/api.rb
@@ -1,4 +1,30 @@
 class Coingecko::API
+  attr_accessor :endpoint
+  
+  def self.api_call(endpoint="coins")
+    #defaults to list coins
+    @endpoint = endpoint
+    res = open("https://api.coingecko.com/api/v3/#{endpoint}").read 
+    @json = JSON.parse(res)
+    #puts @json.class
+    
+  end
+  
+  # def json 
+  #   @json
+  # end 
+  
+  def self.find_coin(query)
+    #binding.pry
+    self.api_call("coins/list")
+    coin = []
+    @json.each_with_index do |v,k| 
+      coin << v if v.fetch("name").include?(query) #needs to refactor this one
+    end 
+    
+    #coin.empty? ? "Your search returned no results, please try again." : coin 
+    puts coin
+  end 
   
   
-end   
+end 
--- lib/coingecko/cli.rb
+++ lib/coingecko/cli.rb
@@ -12,7 +12,7 @@ class Coingecko::CLI
   end 
   
   def selection 
-    puts "Which coin would you like to check? For the main menu please type menu"
+    puts "What would you like to do? For the main menu please type menu."
     @input = gets.strip.downcase 
     #@input
     
@@ -23,9 +23,13 @@ class Coingecko::CLI
     @input
   end 
   
+  def query
+    @query
+  end  
+  
   def main_menu
-    puts "\n-To list the top 20 coins type ls."
-    puts "-For a list of ALL coins please type all"
+    puts "\n-To list the top 50 coins type ls."
+    puts "-To find a coin by name, type find."
     puts "-To QUIT: please type q."
 
     self.selection
@@ -41,7 +45,7 @@ class Coingecko::CLI
   
   def list_all
       
-     puts "listing ALL coins...tbd"
+     puts "listing *ALL* coins...tbd"
      #list coins.  
 
     self.selection
@@ -51,17 +55,30 @@ class Coingecko::CLI
       case input
       when "ls", "list"
         self.list
-      when "menu", "m"
+      when "menu", "m", "back", "b"
         self.main_menu
       when "q", "quit", "exit", "exit!"
         self.quit  
-        
-      # need logic ike when input.include? blblabla check if it exists  
+      when "find", "f"  
+        find
+      # NEED logic ike when input.include? blblabla check if it exists  
     else   
       puts "omg"
       selection
     end
   end  
+  
+  def find 
+    puts "Which coin would to find? To go back, please type back."
+    @query = gets.strip.downcase 
+   
+    if query == "back" || query == "b"
+      self.selection
+    else     
+    find_query = Coingecko::API.find_coin(query)
+    find_query
+   end 
+  end 
     
   def quit
     puts "Goodbye! See you next time"
